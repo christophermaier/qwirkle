d1198ec1b08313673a74e2bdbb539bf157a04140
Christopher Maier <christopher.maier@gmail.com>
2015-06-13 17:00:08 -0400

    Add some dev deps

--- project.clj
+++ project.clj
@@ -5,5 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [midje "1.6.3"]
-                 [org.clojure/test.check "0.7.0"]
-                 [org.clojure/core.logic "0.8.10"]])
+                 [org.clojure/test.check "0.7.0"]]
+  :profiles {:dev {:plugins [[lein-marginalia "0.8.0"]
+                             [cider/cider-nrepl "0.8.2"]
+                             [lein-cljfmt "0.1.10"]]}})
